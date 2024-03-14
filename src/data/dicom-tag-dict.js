9dab5ff197aa3a996969501da66142f7181fc353
Ryan Macdonald <macdonaldr93@gmail.com>
2024-03-14 16:52:05 -0400

    feat: add tags for pixel information

--- src/data/dicom-tag-dict.js
+++ src/data/dicom-tag-dict.js
@@ -1,5 +1,6 @@
 const DICOM_TAG_DICTIONARY = {
   AccessionNumber: 'x00080050',
+  Columns: 'x00280011',
   ImageType: 'x00080008',
   InstitutionAddress: 'x00080081',
   InstitutionName: 'x00080080',
@@ -7,9 +8,13 @@ const DICOM_TAG_DICTIONARY = {
   PatientID: 'x00100020',
   PatientName: 'x00100010',
   PatientSex: 'x00100040',
+  PixelData: 'x7fe00010',
   ReferringPhysicianName: 'x00080090',
+  Rows: 'x00280010',
   SOPClassUID: 'x00080016',
   SOPInstanceUID: 'x00080018',
+  WindowCenter: 'x00281050',
+  WindowWidth: 'x00281051',
 };
 
 const DICOM_COMMON_TAG_DICTIONARY = {
